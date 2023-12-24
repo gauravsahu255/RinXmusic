7b577da0073cd0da4b7808444de2284ba9b27f78
gauravsahu255 <154047872+gauravsahu255@users.noreply.github.com>
2023-12-24 18:51:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/anime_x_god")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/savage_bot_support")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/anime_x_god_group")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
